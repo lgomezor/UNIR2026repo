2e2f421ab0f6d96bd2ec9b8d818f1c7aca9ab4ba
Lucía Gómez <luciagomezbio@gmail.com>
2026-04-18 17:25:05 +0200

    Añadido footer a main y app

--- Ejercicios_propuestos/t2-components-cine/src/App.jsx
+++ Ejercicios_propuestos/t2-components-cine/src/App.jsx
@@ -5,9 +5,18 @@ function App() {
 
 
   return (
-    <>
+      <div className="app-layout">
 
-    </>
+        <Menu/>
+        {/* <MenuProps backgroundColor="red"/> */}
+        {/* <MenuPropsDestructuring backgroundColor="red"/> */}
+
+        <MainContent/>
+        {/* <MainContentFragmentExplicito/> */}
+        {/* <MainContentFragmentImplicito/> */}
+
+        <Footer/>
+      </div>
   )
 }
 
